ae193ecf6d764a2fb0b1bcb117f306fa6dbf4f10
Samuel Aleixo <samuelmatriz@hotmail.com>
2022-06-10 11:54:30 -0300

    Adicionar arquivos de projeto.

---
+++ NetLoginSimples/Areas/Identity/Data/AplicacaoUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace NetLoginSimples.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AplicacaosUser class
+public class AplicacaoUser : IdentityUser
+{
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+}
+
